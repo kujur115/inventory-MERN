faaf6267293a567c7b02c4b6dc2633f7d3d5906e
Pratik Kujur <kujurpratik115@gmail.com>
2024-01-14 00:04:19 +0530

    merge client into subdirectory

---
+++ client/src/App.js
@@ -0,0 +1,12 @@
+import Dashboard from "./components/Dashboard";
+
+const App = () => {
+  return (
+    <div className="App">
+      <h1> Inventory Management System </h1>
+      <Dashboard />
+    </div>
+  );
+};
+
+export default App;
