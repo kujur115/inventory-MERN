84d609aaf7579e18dbf6e1290159fe67ea8ceda5
Pratik Kujur <kujurpratik115@gmail.com>
2024-03-05 21:17:31 +0530

    Added multer for file uploads.

--- server/routes/products/index.js
+++ server/routes/products/index.js
@@ -7,11 +7,12 @@ const {
   updateProduct,
 } = require("../../controllers/productsController");
 const protect = require("../../middleWare/authMiddleware");
+const { upload } = require("../../utils/fileUpload");
 const router = express.Router();
 
-router.post("/", protect, createProduct);
+router.post("/", protect, upload.single("image"), createProduct);
 router.get("/", protect, getProducts);
-router.patch("/:id", protect, updateProduct);
+router.patch("/:id", protect, upload.single("image"), updateProduct);
 router.get("/:id", protect, singleProduct);
 router.delete("/:id", protect, deleteProduct);
 
